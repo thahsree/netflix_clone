5f1d90901825932bc8d9cc9f5909866b71b9afae
thashreef <chthashreef22@gmail.com>
2023-07-26 23:57:04 +0530

    New SignUp Page

--- src/components/Signup/SignUp.js
+++ src/components/Signup/SignUp.js
@@ -1,15 +1,56 @@
 import React from 'react'
+import './SignUp.css'
 
 function SignUp() {
   return (
-    <div>
+   <div className='signUpMain'>
+     <div className='signUpParentDiv'>
       <form>
-        <div>
-            
-            
-        </div>
+        <label>UserName</label>
+        <br/>
+        <input 
+         className='input'
+         type='text'
+         id='fname'
+         name='name'
+         defaultValue="John"
+        />
+        <br/>
+        <label htmlFor="fname">Email</label>
+          <br />
+          <input
+            className="input"
+            type="email"
+            id="fname"
+            name="email"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="lname">Phone</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            id="lname"
+            name="phone"
+            defaultValue="Doe"
+          />
+          <br />
+          <label htmlFor="lname">Password</label>
+          <br />
+          <input
+            className="input"
+            type="password"
+            id="lname"
+            name="password"
+            defaultValue="Doe"
+          />
+          <br />
+          <br />
+          <button>Signup</button>
       </form>
     </div>
+   </div>
   )
 }
 
