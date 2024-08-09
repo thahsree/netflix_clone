bfd1af9756f162ee9523e8b0c7c748f08b2624b2
thashreef <chthashreef22@gmail.com>
2024-08-09 18:59:06 +0400

    Added vercel.json

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Login from '../components/Login/Login'
 
 function login() {
   return (
-    <div>
-      <Login></Login>
-    </div>
+    <>
+      LOGIN
+    </>
   )
 }
 
